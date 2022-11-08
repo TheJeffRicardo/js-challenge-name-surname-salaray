65f03869979db79eb6aedc22b1ff6548ae11a48c
TheJeffRicardo <konkemalindi@gmail.com>
2022-11-08 10:22:40 +0200

    JS challenge

---
+++ js/code.js
@@ -0,0 +1,10 @@
+
+let firstName = prompt("Name: ")
+
+let surName = prompt("Surname: ")
+
+let salary = parseInt(prompt("Salary: R"))
+
+let sentence = `My name is ${firstName} ${surName} and I earn R${eval(salary+500)}`
+
+document.write(sentence) 
